56f5bb1a5f75095383fb66399add1e4c2af0ba7a
yeeamanda <amanda.yee99@gmail.com>
2021-02-12 23:57:36 -0500

    updated js and css style

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,1 +1,30 @@
 /* Put your javascript in here */
+/*const imagegallery = [];
+imagegallery.forEach(element =>{
+	let i = 0;
+  const imagecount = document.querySelectorAll('li').length;
+  if (i<imagecount) {
+		i++;
+    imagegallery.push(document.querySelectorAll('li')[i].src);
+    }
+})*/
+
+const imagegallery = [];
+for (let i = 0; i < document.querySelectorAll('li').length; i++) {
+  imagegallery.push(document.querySelectorAll('li')[i].src);
+}
+console.log(imagegallery)
+
+
+let currentSlideIndex = -1;
+
+function clickforward() {
+    const arrowright = document.querySelector('arrowright');
+    currentSlideIndex +=;
+    if (currentSlideIndex < 3) {
+   		list.style.marginLeft += '130px';
+    }
+    
+arrowleft.addEventListener('click', (event)=> {
+	clickforward()
+  })
