305765068fcd2279886c6bd63135ba1d2eeae336
yeeamanda <amanda.yee99@gmail.com>
2021-02-13 17:51:27 -0500

    updated js functionality, added html id tags

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,22 +1,40 @@
-/* Put your javascript in here */
-
-const imagelist = document.querySelector('ul');
-
 const imagegallery = [];
+// eslint-disable-next-line linebreak-style
+
+// eslint-disable-next-line no-plusplus
 for (let i = 0; i < document.querySelectorAll('li').length; i++) {
   imagegallery.push(document.querySelectorAll('img')[i].src);
 }
+let SlideIndex = -1;
 
-let currentSlideIndex = -1;
+document.querySelector('.right').onclick = () => {
+  // eslint-disable-next-line no-const-assign
+  SlideIndex += 3;
+  if (SlideIndex > 5) {
+    // eslint-disable-next-line no-const-assign
+    SlideIndex = 6;
+  } else if (SlideIndex == 5) {
+    document.getElementById('image1').src = imagegallery[SlideIndex - 1];
+    document.getElementById('image2').src = imagegallery[SlideIndex];
+    document.getElementById('image3').src = imagegallery[SlideIndex + 1];
+  } else {
+    document.getElementById('image1').src = imagegallery[SlideIndex + 1];
+    document.getElementById('image2').src = imagegallery[SlideIndex + 2];
+    document.getElementById('image3').src = imagegallery[SlideIndex + 3];
+  }
+};
 
-function clickforward() {
-    const arrowright = document.querySelector('arrowright');
-    currentSlideIndex +=;
-    if (currentSlideIndex < 3) {
-   		list.style.marginLeft += '130px';
-    }
-}
-arrowleft.addEventListener('click', (event)=> {
-	clickforward()
-  })
-
+document.querySelector('.left').onclick = () => {
+  SlideIndex -= 3;
+  if (SlideIndex < -1) {
+    SlideIndex = 0;
+  } else if (SlideIndex == -1) {
+    document.getElementById('image1').src = imagegallery[SlideIndex + 1];
+    document.getElementById('image2').src = imagegallery[SlideIndex + 2];
+    document.getElementById('image3').src = imagegallery[SlideIndex + 3 ];
+  } else {
+    document.getElementById('image1').src = imagegallery[SlideIndex - 1];
+    document.getElementById('image2').src = imagegallery[SlideIndex];
+    document.getElementById('image3').src = imagegallery[SlideIndex + 1];
+  }
+};
