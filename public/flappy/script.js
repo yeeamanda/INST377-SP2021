e60c5b58b042e00cd7ea8bce010bfa4788770894
yeeamanda <amanda.yee99@gmail.com>
2021-02-18 16:35:50 -0500

    changed comma

--- public/flappy/script.js
+++ public/flappy/script.js
@@ -18,7 +18,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // setInterval runs startGame every 20 ms
     // Gives the gravity effect by dropping by 2 each run (in function)
     // setting into timerID gives us a way to stop it later (using clearInterval)
-    let timerId = setInterval(startGame. 20)
+    let timerId = setInterval(startGame, 20);
 
     // adding 50 to the bird height on jump
     function jump() {
@@ -26,5 +26,5 @@ document.addEventListener('DOMContentLoaded', () => {
         bird.style.bottom = birdBottom + 'px'
     }
     // on key up, do the jump function
-    document.addEventListener('keyup', jump)
+    document.addEventListener('keyup', jump);
 });
