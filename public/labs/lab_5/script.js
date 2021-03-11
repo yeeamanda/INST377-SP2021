f892b3d04d6a9e509fdca13cf7daf57ed7697ad5
yeeamanda <amanda.yee99@gmail.com>
2021-03-11 15:57:09 -0500

    changed title and added js from A1

--- public/labs/lab_5/script.js
+++ public/labs/lab_5/script.js
@@ -6,7 +6,46 @@ function mapInit() {
 async function dataHandler(mapObjectFromFunction) {
   // use your assignment 1 data handling code here
   // and target mapObjectFromFunction to attach markers
+// script starts here (from A1)
+const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
+const foodPlace = [];
+fetch(endpoint).then(blob => blob.json())
+.then(data => foodPlace.push(...data))
+
+
+function findMatches(searchQuery){
+    return foodPlace.filter(place => {
+        const regex = new RegExp(searchQuery, 'gi'); //regExp is an object that goes into .match method
+        return place.city.match(regex) || place.category.match(regex)|| place.name.match(regex);
+    });
 }
+
+function displayMatches(){
+    const matchArr = findMatches(this.value); //this.value is the data being input in the form
+    const html = matchArr.map(palce => { //.map creates an array with equal size but replaces the values with this instead
+        return `
+        <li>
+            <span class="name">${place.name}</span>
+            <span class="name">${place.city}</span>
+            <span class="name">${place.rodent_and_insects}</span>
+        </li>
+        `;
+
+    }).join(''); //This changes html from an array to a big string
+
+    suggestions.innerHTML = html; //takes the html strong from html and creates html in this element
+}
+
+const searchInput = document.querySelector('.search'); //This chooses an element with the class search
+const suggestions = document.querySelector('.suggestions'); //Chooses element with class suggestions
+
+searchInput.addEventListener('keyup', displayMatches);
+window.onload = windowActions;
+
+
+
+}
+
 
 async function windowActions() {
   const map = mapInit();
@@ -14,3 +53,5 @@ async function windowActions() {
 }
 
 window.onload = windowActions;
+
+
