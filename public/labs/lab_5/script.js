8981eafdeee72cc7458eafee7bc51ecb42514754
yeeamanda <amanda.yee99@gmail.com>
2021-03-12 19:43:49 -0500

    add filtering and top 5 code chunk

--- public/labs/lab_5/script.js
+++ public/labs/lab_5/script.js
@@ -1,8 +1,7 @@
 function mapInit() {
   // follow the Leaflet Getting Started tutorial here
-  const map = L.map('mapid').setView([51.505, -0.09], 13);
-
-  L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+  let map = L.map('mapid').setView([38.937661,-76.944736], 13);
+   L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox/streets-v11',
@@ -10,6 +9,7 @@ function mapInit() {
     zoomOffset: -1,
     accessToken: 'pk.eyJ1IjoiYW1hbmRheWVlIiwiYSI6ImNrbTVnNTh6ZzAxZzgycHM5dWdnbXVjN3cifQ.Ikepxu09mwyzzW8zWlT9jQ'
   }).addTo(map);
+  
   return map;
 }
 
@@ -21,17 +21,17 @@ async function dataHandler(mapObjectFromFunction) {
   const request = await fetch(endpoint);
   const mArr = await request.json().then(data => foodPlace.push(...data));
 
-  function findMatches(searchQuery, foodPlace) {
+  /*function findMatches(searchQuery, foodPlace) {
     return foodPlace.filter(place => {
       const regex = new RegExp(searchQuery, 'gi'); //regExp is an object that goes into .match method
       return place.zip.match(regex)
     });
-  };
+  };*/
 
   function displayMatches(event) {
     query = event.target.value;
-    const matchArr = findMatches(query, foodPlace);// this.value is the data being input in the form
-    const html = matchArr.map(place => {// .map makes array with equal size but replaces the values
+    /*const matchArr = findMatches(query, foodPlace);// this.value is the data being input in the form
+    const html = newmatch.map(place => {// .map makes array with equal size but replaces the values
       return `         
           <li class = "box has-background-danger-light">
               <span class="name">${place.name}</span> <br>
@@ -48,7 +48,35 @@ async function dataHandler(mapObjectFromFunction) {
       suggestions.innerHTML = html;// takes html strong from html and creates html in this element
     } else {
       suggestions.innerHTML = '';
+    }*/
+    
+    let myLines = foodPlace.filter((item) => (item.zip.includes(query) && item.geocoded_column_1));
+    const firstFive = myLines.slice(0,5)
+		firstFive.forEach((inst) =>{
+    		const coord = inst.geocoded_column_1.coordinates;
+  			let marker = L.marker([coord[1], coord[0]]).addTo(mapObjectFromFunction);
+				const html = firstFive.map(place => {// .map makes array with equal size but replaces the values
+      return `         
+          <li class = "box has-background-danger-light">
+              <span class="name">${place.name}</span> <br>
+              <address>
+                  ${place.address_line_1}
+              </address>
+              <span class = "zip">${place.zip}</span>
+          </li>           
+      `;
+       }).join(''); // This changes html from an array to a big string
+
+    if(query) {
+      suggestions.innerHTML = html;// takes html strong from html and creates html in this element
+    } else {
+      suggestions.innerHTML = '';
     }
+    
+    });
+    
+    return mapObjectFromFunction;
+    
   };
 
   const searchInput = document.querySelector('.search'); //This chooses an element with the class search
